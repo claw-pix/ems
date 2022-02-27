949e91cd265b6a0cc3ae27150a6bcfd9a7b83db0
ShivamPoniya <70210762+claw-pix@users.noreply.github.com>
2022-02-27 15:58:17 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -15,7 +15,7 @@ public:
 		cout << "\n----------------------------\n Employee Details\n---------------------------\n";
 		cout << setw(10) << "Id" << setw(10) << "Name" << setw(10) << "Age" << setw(15) << "Department" << endl;
 		for (i < 0; i < n; i++){
-			cout << setw(10) << emp[i].Id << setw(10) << emp[i].Name << setw(10) << emp[i].age << setw(15) << emp[i].Dept << endl;
+			cout << setw(10) << emp[i].Id << setw(10) << emp[i].Name << setw(10) << emp[i].age << setw(10) << emp[i].Dept << endl;
 		}
 	}
 	
